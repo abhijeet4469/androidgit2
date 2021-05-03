436fe31b707954488acddad14f202d0ddb2a69f2
admin <abhijeet.patil@flairlabs.com>
2021-05-03 18:27:59 +0530

    Submodule update from git replica

--- src/main/java/com/example/verysdk/VerySdkMainActivity.java
+++ src/main/java/com/example/verysdk/VerySdkMainActivity.java
@@ -3,6 +3,7 @@ package com.example.verysdk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class VerySdkMainActivity extends AppCompatActivity {
 
@@ -11,6 +12,10 @@ public class VerySdkMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_verysdk);
 
+
         // sample comment to check for submodule commit
+
+        TextView textId = findViewById(R.id.textId);
+        // sample commit from another project og Git replica
     }
 }
