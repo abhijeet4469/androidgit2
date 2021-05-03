db587ebb9fc39443ee540368220000565bae8a18
admin <abhijeet.patil@flairlabs.com>
2021-05-03 17:36:05 +0530

    submodule commit

--- src/main/java/com/example/verysdk/VerySdkMainActivity.java
+++ src/main/java/com/example/verysdk/VerySdkMainActivity.java
@@ -10,5 +10,7 @@ public class VerySdkMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_verysdk);
+
+        // sample comment to check for submodule commit
     }
 }
